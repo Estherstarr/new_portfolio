7bc8e9fa3bd7b1bef7e53b321000f7d164ca4a47
Onomeh Onwuanibe <60798978+Estherstarr@users.noreply.github.com>
2023-06-13 15:10:06 +0000

    Set configurations

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+export default function App() {
+  return (
+    <div>
+      <nav>
+        <div>
+          <h2>My Logo</h2>
+        </div>
+        <div>
+          <h3>Portfolio</h3>
+          <h3>About</h3>
+          <h3>Contact</h3>
+        </div>
+      </nav>
+      <div>
+      <h1 className="text-2xl font-bold">I'm Onomeh, 
+      Glad to see you! </h1>
+      <p>
+      I’m a software developer! I can help you build a product , feature
+      or website. Look through some of my work and experience! 
+      I am always thinking about code and this passion often leads me to solve my coding problem, while I’m jogging, playing keyboard, or cooking .
+      </p>
+      </div>
+    </div>
+  )
+}
