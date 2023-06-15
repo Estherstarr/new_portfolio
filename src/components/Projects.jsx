8dc8295ca5901afb6796b031c40830c6c0c47b4b
Onomeh Onwuanibe <60798978+Estherstarr@users.noreply.github.com>
2023-06-15 20:25:08 +0000

    Add projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+        <div  className="w-2/3 mx-auto flex mt-28 rounded-lg bg-white">
+          <div className="pt-3 pl-10"><img src="#"/></div>
+          <div className="pt-3">
+                <h2>Tonic</h2>
+                <ul>
+                <li>CANOPY</li>
+                <li>Back end Dev</li>
+                <li>2015</li>
+                </ul>
+                <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>JavaScript</li>
+                </ul>
+                <a href="https://estherstarr.github.io/conferencePage/" >See Project</a>
+          </div>
+       </div>
+    </div>
+  )
+}
+
+export default Projects
+
+
+
