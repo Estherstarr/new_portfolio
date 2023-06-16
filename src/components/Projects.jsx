e0716c1a6a1519f20dbb0be224fc42e4e282c028
Onomeh Onwuanibe <60798978+Estherstarr@users.noreply.github.com>
2023-06-16 20:43:44 +0000

    create project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 
-const Projects = () => {
+const Projects = ({image_url,project_title,project_name,stack,year,project_description,stack_1,stack_2,stack_3,project_url}) => {
   return (
     <div>
-        <div  className="w-2/3 mx-auto flex mt-28 rounded-lg bg-white">
-          <div className="pt-3 pl-10"><img src="#"/></div>
-          <div className="pt-3">
-                <h2>Tonic</h2>
-                <ul>
-                <li>CANOPY</li>
-                <li>Back end Dev</li>
-                <li>2015</li>
+        <div  className="w-2/3 mx-auto p-12 flex justify-between mt-28 rounded-xl bg-white shadow-lg">
+          <div className="pt-3 pl-10 rounded-xl w-5/12 shadow-sm "><img src={image_url}/></div>
+          <div className="pt-3 w-7/12 ml-10">
+                <h2>{project_title}</h2>
+                <ul className="flex">
+                <li>{project_name}</li>
+                <li>{stack}</li>
+                <li>{year}</li>
                 </ul>
-                <ul>
-                <li>html</li>
-                <li>css</li>
-                <li>JavaScript</li>
+                <p>{project_description}</p>
+                <ul className="flex">
+                <li>{stack_1}</li>
+                <li>{stack_2}</li>
+                <li>{stack_3}</li>
                 </ul>
-                <a href="https://estherstarr.github.io/conferencePage/" >See Project</a>
+                <a href={project_url}>See Project</a>
           </div>
        </div>
     </div>
